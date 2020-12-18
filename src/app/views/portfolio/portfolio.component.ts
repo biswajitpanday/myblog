1fd97d1c3c2083a65f0d7e499e7450941add2844
biswajitmailid <biswajitmailid@gmail.com>
2020-12-18 11:10:09 +0000

    Portfolio Card Design.

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  portfolioList: IPortfolio[];
+  defaultImage: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.getPortfolio();
+    this.defaultImage = "../../../assets/img/default-thumbnail.jpg"
   }
 
+  getPortfolio() {
+    this.portfolioList = [
+      {
+        title: "Subscriber Verification System (SVS)",
+        description: "",
+        image: "",
+        shortDescription: "",
+        siteLink: "",
+        companyName: "Robi",
+        companyLocation: "Dhaka, Bangladesh",
+        started: new Date(),
+        ends: new Date()
+      },
+      {
+        title: "DairyApplication",
+        description: "",
+        image: "",
+        shortDescription: "",
+        siteLink: "",
+        companyName: "Tmr Feeding",
+        companyLocation: "Netherlands",
+        started: new Date(),
+        ends: new Date()
+      }
+    ]
+  }
+
+}
+
+export interface IPortfolio {
+  title: string;
+  shortDescription: string;
+  description: string;
+  image: string;
+  siteLink: string;
+  companyName: string;
+  companyLocation: string;
+  started: Date;
+  ends: Date;
 }
