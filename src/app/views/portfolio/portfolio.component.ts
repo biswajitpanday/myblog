b3889b020270067574aa2e67d820dffaa97f8f80
biswajitmailid <biswajitmailid@gmail.com>
2020-12-18 11:58:18 +0000

    Portfolio functionality.

--- src/app/views/portfolio/portfolio.component.ts
+++ src/app/views/portfolio/portfolio.component.ts
@@ -16,41 +16,77 @@ export class PortfolioComponent implements OnInit {
     this.defaultImage = "../../../assets/img/default-thumbnail.jpg"
   }
 
+  goToLink(url: string) {
+    window.open(url, "_blank");
+  }
+
   getPortfolio() {
     this.portfolioList = [
       {
         title: "Subscriber Verification System (SVS)",
+        technologies: ["C#", "ASP.NET Core 2.2", "JavaScript", "JQuery", "Oracle"],
         description: "",
         image: "",
         shortDescription: "",
-        siteLink: "",
+        siteLink: "https://svsweb.robi.com.bd/",
+        employer: "KAZ Software",
         companyName: "Robi",
         companyLocation: "Dhaka, Bangladesh",
         started: new Date(),
         ends: new Date()
       },
       {
-        title: "DairyApplication",
+        title: "JoyList",
+        technologies: ["Node.js", "React", "MongoDb", "AWS EC2", "AWS S3", "AWS Cloud Formation"],
         description: "",
         image: "",
         shortDescription: "",
         siteLink: "",
+        employer: "KAZ Software",
+        companyName: "JoyList",
+        companyLocation: "Dhaka, Bangladesh",
+        started: new Date(),
+        ends: new Date()
+      },
+      {
+        title: "DairyApplication",
+        technologies: ["C#", "ASP.NET","Angular.Js", "ASP.NET","Angular.Js"],
+        description: "",
+        image: "",
+        shortDescription: "",
+        siteLink: "",
+        employer: "KAZ Software",
         companyName: "Tmr Feeding",
         companyLocation: "Netherlands",
         started: new Date(),
         ends: new Date()
-      }
+      },
+      {
+        title: "Shopway",
+        technologies: ["Node.js", "Angular 7", "MongoDb", "AWS EC2", "AWS S3", "AWS EBS"],
+        description: "",
+        image: "",
+        shortDescription: "",
+        siteLink: "",
+        employer: "Independent",
+        companyName: "Shopway",
+        companyLocation: "Dhaka, Bangladesh",
+        started: new Date(),
+        ends: new Date()
+      },
+
     ]
   }
-
 }
 
 export interface IPortfolio {
   title: string;
   shortDescription: string;
+  technologies: string[];
   description: string;
   image: string;
   siteLink: string;
+  employer: string;
   companyName: string;
   companyLocation: string;
   started: Date;
