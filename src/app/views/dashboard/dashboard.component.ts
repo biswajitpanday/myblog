cc023c3d595e0211bcebf28adc07e9ed5d04988a
biswajitmailid <biswajitmailid@gmail.com>
2020-12-19 18:11:53 +0000

    Fix Skills.

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -8,8 +8,12 @@ export class DashboardComponent implements OnInit {
     javascript: any;
     nodejs: any;
     android: any;
+    professionalSkills: IProfessionalSkills[];
+    otherSkills: string[];
 
     constructor() {
+        this.professionalSkills = [];
+        this.otherSkills = [];
     }
 
     ngOnInit() {
@@ -17,5 +21,53 @@ export class DashboardComponent implements OnInit {
         this.javascript = 90;
         this.nodejs = 70;
         this.android = 60;
+        this.getSkills();
+        this.getProfessionalSkills();
     }
+
+    getSkills () {
+        this.otherSkills = ["C#", "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web API", "Entity Framework", "MSSQL Server", "Oracle", "LINQ", "Log4Net", "NUnit", "Node.js", "Express.js", "Hapi", "MongoDB", "JavaScript", "TypeScript", "JQuery", "ReactJS", "Redux", "Angular", "Angular.js", "Electron", "HTML5", "Bootstrap", "Trello", "Jira", "Git", "Xamarin", "Design Patterns", "Microservices", "Onion Architecture", "n-tier", "3-tire"];
+    }
+
+    getProfessionalSkills() {
+        this.professionalSkills = [
+            {
+                stack: ".NET (C#)",
+                yearsOfExperience: 5,
+                expertiseLabel: "Expert",
+                levelPercentage: 90
+            },
+            {
+                stack: "ASP.NET",
+                yearsOfExperience: 5,
+                expertiseLabel: "Expert",
+                levelPercentage: 85
+            },
+            {
+                stack: ".NET Core & ASP.Net Core",
+                yearsOfExperience: 2,
+                expertiseLabel: "Expert",
+                levelPercentage: 80
+            },
+            {
+                stack: "JavaScript",
+                yearsOfExperience: 6,
+                expertiseLabel: "Expert",
+                levelPercentage: 90
+            },
+            {
+                stack: "Node.js",
+                yearsOfExperience: 3,
+                expertiseLabel: "Intermediate",
+                levelPercentage: 80
+            }
+        ]
+    }
+}
+
+export interface IProfessionalSkills {
+    stack: string;
+    levelPercentage: number;
+    yearsOfExperience: number;
+    expertiseLabel: string;
 }
