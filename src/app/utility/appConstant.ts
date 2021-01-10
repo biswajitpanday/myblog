c62612bd63e2ec7abc30756ec68abf0811a4a10e
biswajitmailid <biswajitmailid@gmail.com>
2021-01-10 23:12:05 +0000

    Appconstant add.

--- src/app/utility/appConstant.ts
+++ src/app/utility/appConstant.ts
@@ -1,3 +1,9 @@
 export class AppConstant {
     static authToken = 'auth_token';
+    static thailandCC = "Th";
+    static bangladeshCC = "Bd";
+    static indiaCC = "In"
+    static thailand = "thailand";
+    static bangladesh = "bangladesh";
+    static india = "india"
 }
